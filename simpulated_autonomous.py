9a93fa3b44ac95e118b50235b2035afc510dc42f
Brian Kaether <briankaether@DN0a22a67b.SUNet>
2018-11-10 14:41:38 -0800

    code actually works now

--- simpulated_autonomous.py
+++ simpulated_autonomous.py
@@ -16,24 +16,24 @@ port = 8870
 pub_angle = Publisher(fields, format_, port)
 
 i = 0
-point = [ (37.429, -122.170) ]
+point = (37.429, -122.170)
 
-pt_click = [(37.428, -122.171)]
+pt_click = (37.428, -122.171)
 
 i = 0
 angles = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
 
 while True:
-    lat,lon = point
+    lat,lon = point[0], point[1]
     lat_click, lon_click = pt_click
     angle = angles[i]
     print lat, lon
     print lat_click, lon_click
     print angle
 
-    i = (i+1) % len(points)
+    i = (i+1) % len(angles)
     pub.send(0, 4, lat, lon, 0, 0, 0, 0)
     pub_click.send(lat_click, lon_click)
     pub_angle.send(angle)
 
-    time.sleep(1)
+    time.sleep(3)
