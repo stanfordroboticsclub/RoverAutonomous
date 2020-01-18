202b80aa5bc7394633b25b28c37fcde3565656cf
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-01-17 18:34:33 -0800

    added qr code

---
+++ QR_locations.py
@@ -0,0 +1,30 @@
+
+
+
+
+import UDPComms
+
+
+qr_codes = UDPComms.Subscriber()
+
+
+
+
+
+
+msg = qr_codes.recv()
+
+
+
+# msg = { 'lat': float, "lon": float, "marker_num": int }
+
+
+
+class ParticleFilter:
+
+
+    def __init__(self):
+        pass
+
+
+
