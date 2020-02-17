e24104cf69bf94c0f04bcd4e98969bce7e04e180
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-02-17 02:59:53 -0800

    moving code in

--- autonomy.py
+++ autonomy.py
@@ -16,35 +16,157 @@ class Situation(enum.Enum):
     done = enum.auto()
 
 class StateMachine:
-    def __init__(self, rover):
+    def __init__(self, parent, rover):
+        self.parent = parent
         self.rover = rover
-
-        self.PathFollower  = PathFollower(rover, self)
-        self.Search        = Search(rover, self)
-        self.FinalApproach = FinalApproachPost(rover, self)
-
-        self.states = [ s(rover) for s in [PathFollower, Search, FinalApproachPost]]
-        self.state = self.PathFollower
-
-    def change_state(self, state):
-        self.state = state
 
     def run(self) -> Situation:
         raise NotImplementedError 
 
 
-class GateTask(StateMachine):
+class PostTask(StateMachine):
+
+    def __init__(self,*args):
+        super().__init__(*args)
+
+        self.pathfollower = PathFollower(self, self.rover)
+        self.finalapproach = FinalApproachPost(self, self.rover)
+
+        self.state = self.pathfollower
+
     def run(self) -> Situation:
         self.rover.update()
 
 
 class PathFollower(StateMachine):
 
+    def __init__(self,*args):
+        super().__init__(*args)
+        self.lookahead_radius = 6
+
+
     def run(self) -> Situation:
         self.rover.update()
 
         if ()
             return Situation.done
+
+    def find_lookahead(self,waypoints):
+        start_waypoints = [self.start_point] + waypoints
+        waypoint_pairs = zip(start_waypoints[::-1][1:], start_waypoints[::-1][:-1])
+        i = 0
+        for p1, p2 in waypoint_pairs:
+            lookahead = self.lookahead_line(p1,p2)
+            if lookahead is not None:
+                print("point intersection", i)
+                return lookahead
+            i+=1
+        else:
+            print("NO intersection found!")
+
+        distances = []
+        for p1, p2 in waypoint_pairs:
+            lookahead = self.lookahead_line(p1,p2,project = True)
+            if lookahead is not None:
+                distances.append( (self.distance(lookahead), lookahead) )
+
+        for p1 in waypoints:
+            point = self.project(p1['lat'], p1['lon'])
+            distances.append( (self.distance(point), point) )
+
+        return min(distances)[1]
+
+    def distance(self, p1):
+        x_start, y_start = p1
+        # self.robot = self.gps.get()
+        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
+        return math.sqrt((x_start - x_robot)**2 + (y_start - y_robot)**2)
+
+    def lookahead_line(self,p1, p2, project=False):
+        x_start, y_start = self.project(p1['lat'], p1['lon'])
+        x_end, y_end = self.project(p2['lat'], p2['lon'])
+
+        # self.robot = self.gps.get()
+        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
+
+        if (x_robot-x_end)**2 + (y_robot - y_end)**2 < self.lookahead_radius**2:
+            return (x_end, y_end)
+
+        a = (x_end - x_start)**2 + (y_end - y_start)**2
+        b = 2*( (x_end-x_start)*(x_start-x_robot) + (y_end-y_start)*(y_start-y_robot))
+        c = (x_start-x_robot)**2 + (y_start-y_robot)**2 - self.lookahead_radius**2
+
+        if b**2 - 4*a*c <=0:
+            if project:
+                t = -b/(2*a)
+            else:
+                return None
+        else:
+            t = (-b + math.sqrt(b**2 - 4*a*c))/(2*a)
+
+        if not t<=1:
+            return None
+        if not t>=0:
+            return None
+
+        x_look = t * x_end + (1-t) * x_start
+        y_look = t * y_end + (1-t) * y_start
+
+        return (x_look, y_look)
+
+
+    def get_angle(self, lookahead):
+        heading_deg = self.imu.get()['angle'][0]
+        head_rad = math.radians(heading_deg)
+
+        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
+        x_look, y_look = lookahead
+
+        target = math.atan2( x_look - x_robot, y_look - y_robot)
+
+        # anti clockwise positive
+        return ((target - head_rad + math.pi)%(2*math.pi) - math.pi)
+
+    def send_velocities_slow(self, angle):
+        # turn_rate = -100*math.tanh(1*angle)
+        turn_rate = -200*angle/math.pi
+
+        if math.fabs(angle) < math.radians(10):
+            forward_rate = 90
+        elif math.fabs(angle) < math.radians(60):
+            forward_rate = 50
+        elif math.fabs(angle) < math.radians(140):
+            forward_rate = 30
+        else:
+            forward_rate = 0
+
+        out = {"f": forward_rate, "t": turn_rate }
+
+        print(out)
+        self.cmd_vel.send(out)
+
+    def send_velocities(self, angle):
+        # turn_rate = -100*math.tanh(1*angle)
+        turn_rate = -200*angle/math.pi
+
+        if math.fabs(angle) < math.radians(10):
+            forward_rate = 130
+        elif math.fabs(angle) < math.radians(60):
+            forward_rate = 80
+        elif math.fabs(angle) < math.radians(140):
+            forward_rate = 30
+        else:
+            forward_rate = 0
+
+        out = {"f": forward_rate, "t": turn_rate }
+
+        print(out)
+        self.cmd_vel.send(out)
+
+    def send_stop(self):
+        out = {"f": 0, "t": 0 }
+        print('stoping', out)
+        self.cmd_vel.send(out)
 
 class FinalApproachGate(StateMachine):
     pass
@@ -63,7 +185,17 @@ class Search(StateMachine):
 class Rover:
 
     def __init__(self):
-        pass
+        self.imu = Subscriber(8220, timeout=1)
+        self.gps = Subscriber(8280, timeout=2)
+        self.auto_control = Subscriber(8310, timeout=5)
+
+        self.cmd_vel = Publisher(8830)
+        self.lights = Publisher(8590)
+
+        self.aruco = Subscriber(9021, timeout=2)
+        time.sleep(3)
+
+        self.start_point = {"lat":self.gps.get()['lat'] , "lon":self.gps.get()['lon']}
 
     def send_velocities(self):
         pass
@@ -81,20 +213,7 @@ class Rover:
 class Pursuit:
 
     def __init__(self):
-        self.imu = Subscriber(8220, timeout=1)
-        self.gps = Subscriber(8280, timeout=2)
-        self.auto_control = Subscriber(8310, timeout=5)
 
-        self.cmd_vel = Publisher(8830)
-        # self.lights = Publisher(8590)
-        self.servo = Publisher(8120)
-        self.tennis1 = Subscriber(9021, timeout=2)
-        self.tennis2 = Subscriber(9022, timeout=2)
-        self.tennis3 = Subscriber(9023, timeout=2)
-        time.sleep(3)
-
-        self.start_point = {"lat":self.gps.get()['lat'] , "lon":self.gps.get()['lon']}
-        self.lookahead_radius = 6
         self.final_radius = 1.5    # how close should we need to get to last endpoint
         self.search_radius = 20    # how far should we look from the given endpoint
         self.reached_destination = False # switch modes into tennis ball seach
@@ -288,127 +407,6 @@ class Pursuit:
             self.cmd_vel.send(out)
             time.sleep(0.1)
 
-    def find_lookahead(self,waypoints):
-        start_waypoints = [self.start_point] + waypoints
-        waypoint_pairs = zip(start_waypoints[::-1][1:], start_waypoints[::-1][:-1])
-        i = 0
-        for p1, p2 in waypoint_pairs:
-            lookahead = self.lookahead_line(p1,p2)
-            if lookahead is not None:
-                print("point intersection", i)
-                return lookahead
-            i+=1
-        else:
-            print("NO intersection found!")
-
-        distances = []
-        for p1, p2 in waypoint_pairs:
-            lookahead = self.lookahead_line(p1,p2,project = True)
-            if lookahead is not None:
-                distances.append( (self.distance(lookahead), lookahead) )
-
-        for p1 in waypoints:
-            point = self.project(p1['lat'], p1['lon'])
-            distances.append( (self.distance(point), point) )
-
-        return min(distances)[1]
-
-    def distance(self, p1):
-        x_start, y_start = p1
-        # self.robot = self.gps.get()
-        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
-        return math.sqrt((x_start - x_robot)**2 + (y_start - y_robot)**2)
-
-    def lookahead_line(self,p1, p2, project=False):
-        x_start, y_start = self.project(p1['lat'], p1['lon'])
-        x_end, y_end = self.project(p2['lat'], p2['lon'])
-
-        # self.robot = self.gps.get()
-        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
-
-        if (x_robot-x_end)**2 + (y_robot - y_end)**2 < self.lookahead_radius**2:
-            return (x_end, y_end)
-
-        a = (x_end - x_start)**2 + (y_end - y_start)**2
-        b = 2*( (x_end-x_start)*(x_start-x_robot) + (y_end-y_start)*(y_start-y_robot))
-        c = (x_start-x_robot)**2 + (y_start-y_robot)**2 - self.lookahead_radius**2
-
-        if b**2 - 4*a*c <=0:
-            if project:
-                t = -b/(2*a)
-            else:
-                return None
-        else:
-            t = (-b + math.sqrt(b**2 - 4*a*c))/(2*a)
-
-        if not t<=1:
-            return None
-        if not t>=0:
-            return None
-
-        x_look = t * x_end + (1-t) * x_start
-        y_look = t * y_end + (1-t) * y_start
-
-        return (x_look, y_look)
-
-
-    def get_angle(self, lookahead):
-        heading_deg = self.imu.get()['angle'][0]
-        head_rad = math.radians(heading_deg)
-
-        x_robot, y_robot = self.project(self.robot['lat'], self.robot['lon'])
-        x_look, y_look = lookahead
-
-        target = math.atan2( x_look - x_robot, y_look - y_robot)
-
-        # anti clockwise positive
-        return ((target - head_rad + math.pi)%(2*math.pi) - math.pi)
-
-
-
-    def send_velocities_slow(self, angle):
-        # turn_rate = -100*math.tanh(1*angle)
-        turn_rate = -200*angle/math.pi
-
-        if math.fabs(angle) < math.radians(10):
-            forward_rate = 90
-        elif math.fabs(angle) < math.radians(60):
-            forward_rate = 50
-        elif math.fabs(angle) < math.radians(140):
-            forward_rate = 30
-        else:
-            forward_rate = 0
-
-        out = {"f": forward_rate, "t": turn_rate }
-
-        # out = {"f": 70, "t": -150*angle/math.pi }
-        print(out)
-        self.cmd_vel.send(out)
-
-    def send_velocities(self, angle):
-        # turn_rate = -100*math.tanh(1*angle)
-        turn_rate = -200*angle/math.pi
-
-        if math.fabs(angle) < math.radians(10):
-            forward_rate = 130
-        elif math.fabs(angle) < math.radians(60):
-            forward_rate = 80
-        elif math.fabs(angle) < math.radians(140):
-            forward_rate = 30
-        else:
-            forward_rate = 0
-
-        out = {"f": forward_rate, "t": turn_rate }
-
-        # out = {"f": 70, "t": -150*angle/math.pi }
-        print(out)
-        self.cmd_vel.send(out)
-
-    def send_stop(self):
-        out = {"f": 0, "t": 0 }
-        # out = {"f": 70, "t": -150*angle/math.pi }
-        print('stoping', out)
-        self.cmd_vel.send(out)
 
 
 
